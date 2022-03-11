9b1e31bcf6f4313806811b817beeef073090796d
Minna Ronka <minna.ronka@gmail.com>
2022-03-11 17:36:17 +0200

    Add unittest-tests varasto.py coverage 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,44 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_varastoon_ei_liikaa_tavaraa(self):
+        self.varasto.lisaa_varastoon(12)
+
+        #varastossa pitäisi olla saldona tilavuus, joka tässä on 10
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_varastosta_voi_ottaa_vain_saldon_verran(self):
+        self.varasto.lisaa_varastoon(5)
+
+        #varastosta voi ottaa vain 5, koska saldona on 5
+        self.assertAlmostEqual(self.varasto.ota_varastosta(8), 5)
+
+    def test_konstruktoriparametrissa_alkusaldo_negatiivinen_alkusaldoksi_nolla(self):
+        self.varasto2 = Varasto(10,-2)
+
+        #varaston saldon pitäisi olla 0, koska annettu alkusaldo oli negatiivinen
+        self.assertAlmostEqual(self.varasto2.saldo, 0)
+
+    def test_konstruktoriparametrissa_tilavuus_negatiivinen_tilavuudeksi_nolla(self):
+        self.varasto2 = Varasto(-1)
+
+        #varaston tilavuuden pitäisi olla 0, koska annettu tilavuus oli negatiivinen
+        self.assertAlmostEqual(self.varasto2.tilavuus, 0)
+
+    def test_varastoon_ei_voi_lisata_negatiivista_maaraa(self):
+        self.varasto.lisaa_varastoon(-2)
+
+        #varaston saldon pitäisi olla 0, koska lisättävä määrä oli negatiivinen
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_varastosta_ei_voi_ottaa_negatiivista_maaraa(self):
+        self.varasto.ota_varastosta(-2)
+
+        #varaston saldon pitäisi olla 0, koska otettava määrä oli negatiivinen
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_merkkijonoesityksessa_oikeat_tiedot(self):
+
+        #f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+        self.assertAlmostEqual(str(self.varasto),"saldo = 0, vielä tilaa 10")
